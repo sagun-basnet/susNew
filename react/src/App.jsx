4fa5084c7bc1dcbcc917e58bfbc4e41ac62e295b
sagun basnet <basnetsagun8@gmail.com>
2026-03-02 13:36:20 +0545

    usestate

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,12 +1,62 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Button from "./components/Button";
+import { useState } from "react";
+import Form from "./components/Form";
 
 const App = () => {
+  // const [count, setCount] = useState(0);
+  const [bgColor, setBgColor] = useState("");
+  const [status, setStatus] = useState(false);
+
+  // const handleAdd = () => {
+  //   setCount((prev) => prev + 1);
+  //   console.log(count);
+  // };
+
   return (
-    <>
-      <Navbar user={"Jhon deo"} text={"asdfasf asdfasdf asdfasdf"} status={1} />
-    </>
+    <button
+      onClick={() => setStatus(!status)}
+      className="p-2 px-4 rounded-md text-white cursor-pointer bg-red-500"
+    >
+      {status ? "OFF" : "ON"}
+    </button>
+    // <div className="h-screen" style={{ backgroundColor: bgColor }}>
+    //   <div className="bg-transparent flex justify-between px-16 items-center">
+    //     {/* <span>{count}</span>
+    //   <button onClick={handleAdd} className="p-2 px-4 rounded-md bg-blue-500">
+    //     +
+    //   </button> */}
+    //     {/* <Form text={"Register"} /> */}
+    //     <button
+    //       onClick={() => setBgColor("red")}
+    //       className="p-2 px-4 rounded-md text-white cursor-pointer bg-red-500"
+    //     >
+    //       Red
+    //     </button>
+    //     <button
+    //       onClick={() => setBgColor("green")}
+    //       className="p-2 px-4 rounded-md text-white cursor-pointer bg-green-500"
+    //     >
+    //       Green
+    //     </button>
+    //     <button
+    //       onClick={() => setBgColor("blue")}
+    //       className="p-2 px-4 rounded-md text-white cursor-pointer bg-blue-500"
+    //     >
+    //       Blue
+    //     </button>
+    //     <button
+    //       onClick={() => setBgColor("black")}
+    //       className="p-2 px-4 rounded-md text-white cursor-pointer bg-black"
+    //     >
+    //       Black
+    //     </button>
+    //     <button
+    //       onClick={() => setBgColor("yellow")}
+    //       className="p-2 px-4 rounded-md text-white cursor-pointer bg-yellow-500"
+    //     >
+    //       Yellow
+    //     </button>
+    //   </div>
+    // </div>
   );
 };
 
--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 
-const Navbar = ({ user, text, status }) => {
-  console.log(user, text, status);
-
+const Navbar = ({ user }) => {
   return (
     <nav className="px-8 flex items-center justify-between h-20 bg-amber-500">
       <h1 className="text-2xl font-bold">LOGO</h1>
-      {status === 0 ? (
-        <h1>Hello</h1>
+
+      <ul className="flex gap-4">
+        <li>Home</li>
+        <li>About</li>
+        <li>Service</li>
+        <li>Contact</li>
+      </ul>
+
+      {user === "" ? (
+        <div className="flex">
+          <button className="p-2 px-4 rounded-md bg-blue-500">Login</button>
+          <button className="p-2 px-4 rounded-md bg-red-500">Register</button>
+        </div>
       ) : (
-        <ul className="flex gap-4">
-          <li>Home</li>
-          <li>About</li>
-          <li>Service</li>
-          <li>Contact</li>
-        </ul>
+        <span>{user}</span>
       )}
-      <span className="font-bold text-xl">{user}</span>
     </nav>
   );
 };
